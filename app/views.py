750b116ba67137c1bb1e8fa37adcf39b81aa4b7e
Battula, Lakshmana Rao (US - Bengaluru) <lakshmanraob@gmail.com>
2017-02-14 15:35:07 +0530

    update the webhook with weather handling

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template, redirect, url_for, request, abort
+from flask import render_template, redirect, url_for, request, abort, jsonify
+import json
+import urllib.request, urllib.parse, urllib.error
 
 
 @app.route('/')
@@ -70,10 +72,92 @@ def loginAccount():
 @app.route('/webhook', methods=['POST', 'GET'])
 def webhookExp():
     if request.method == 'POST':
-        print ("sample response",request.json)
-        return "Hello user"
+        location = request.json['result']['parameters']['location']
+        if location:
+            print("weather condition required for ..", location)
+            return processLocation(request)
+        else:
+            print("actual json file", request.json)
+            # parsed_json = json.dumps(request.json)
+            # if parsed_json:
+            #     return jsonify({'status': 'success', 'message': parsed_json[10]}), 200
+            # else:
+            #     print("sample response", parsed_json)
+            #     return jsonify({'status': 'success'}), 200
     elif request.method == 'GET':
-        print ("Hello User from custom")
-        return "GET method"
+        return processGetrequest(request)
     else:
         abort(400)
+
+
+def processGetrequest(req):
+    print("Hello User from custom")
+    return jsonify({'status': 'success', 'method': 'GET', 'message': 'This is simple message'}), 200
+
+
+def processLocation(req):
+    if req.json["result"]['action'] != 'weather.search':
+        return {}
+    else:
+        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+        yql_query = makeyqlQuery(req)
+        if yql_query:
+            yql_query = baseurl + urllib.parse.urlencode({'q': yql_query}) + "&format=json"
+            result = urllib.request.urlopen(yql_query).read().decode('utf-8')
+            if result:
+                data = json.loads(result)
+                return extractWeather(data)
+            else:
+                return processErrorrequest(result.getcode())
+        else:
+            return processErrorrequest(206)
+
+
+def makeyqlQuery(req):
+    city = req.json['result']['parameters']['location']
+    if city:
+        return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    else:
+        return processErrorrequest(206, "failed in makeyqlQuery fails")
+
+
+def processErrorrequest(code, msg):
+    return jsonify({'code': code, 'message': errorResponse(code, msg=msg)}), code
+
+
+def errorResponse(code, msg):
+    if code == 400:
+        return "400 BAD request"
+    elif code == 401:
+        return "401 Unauthorized"
+    elif code == 403:
+        return "403 Forbidden"
+    elif code == 404:
+        return "404 Not found"
+    elif code == 500:
+        return "500 Server fault"
+    elif code == 503:
+        return "503 server not available"
+    else:
+        return "webhook call failed with %code% error ", msg
+
+
+def extractWeather(result):
+    query = result['query']
+    if query:
+        result = query['results']
+        if result:
+            channel = result['channel']
+            if channel:
+                units = channel['units']['temperature']
+                city = channel['location']['city'] + "," + channel['location']['country']
+                forecast = channel['item']['condition']
+                speech = "Today in " + city + ' weather is ' + forecast['text'] + ' with temperature of ' + forecast[
+                    'temp'] + units
+                # return jsonify({'status': 'success', 'message': parsed_json[10]}), 200
+                return jsonify({'speech': speech, 'displayText': speech}), 200
+            else:
+                return processErrorrequest(206, "fail in channel")
+        else:
+            return processErrorrequest(206, "fail in result")
+    return processErrorrequest(206, "fail in query")
