4ba9b1422047a61262cd24b4c56af38929198aff
Battula, Lakshmana Rao (US - Bengaluru) <lakshmanraob@gmail.com>
2017-02-18 13:22:20 +0530

    created the seperate class for climate search handling

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from app import app
-from flask import render_template, redirect, url_for, request, abort, jsonify
-import json
-import urllib.request, urllib.parse, urllib.error
+from flask import render_template, redirect, url_for, request, abort
+
 from app.static import myfirebasemodule
+from app.static import climatemodule
 
 
 @app.route('/')
@@ -74,150 +74,15 @@ def loginAccount():
 
 @app.route('/webhook', methods=['POST', 'GET'])
 def webhookExp():
+    climateaction = climatemodule.climateaction()
     if request.method == 'POST':
-        location = getLocation(req=request)
+        location = climateaction.getLocation(req=request)
         if location:
             print("weather condition required for ..", location)
-            return processAction(request)
+            return climateaction.processAction(request)
         else:
             print("actual json file", request.json)
     elif request.method == 'GET':
-        return processGetrequest(request)
+        return climateaction.processGetrequest(request)
     else:
         abort(400)
-
-
-# extract location
-def getLocation(req):
-    location = req.json['result']['parameters']['geo-city']
-    if location:
-        return location
-    else:
-        requestContexts = req.json['result']['contexts']
-        if requestContexts and len(requestContexts) > 0:
-            for reqContext in requestContexts:
-                if reqContext['lifespan'] > 0:
-                    return reqContext['parameters']['geo-city']
-
-
-# Processing the Get request
-def processGetrequest(req):
-    print("Hello User from custom")
-    return jsonify({'status': 'success', 'method': 'GET', 'message': 'This is simple message'}), 200
-
-
-# Processing the location to get the Weather for the extracted location
-def processAction(req):
-    if req.json["result"]["action"] in ['weather.search', 'wind.search']:
-        return processWeatherSearch(req, action=req.json["result"]["action"])
-    elif req.json["result"]['action'] != 'weather.search':
-        return {}
-    else:
-        return processWeatherSearch(req)
-
-
-def processWeatherSearch(req, action):
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeyqlQuery(req)
-    if yql_query:
-        yql_query = baseurl + urllib.parse.urlencode({'q': yql_query}) + "&format=json"
-        result = urllib.request.urlopen(yql_query).read().decode('utf-8')
-        if result:
-            data = json.loads(result)
-            if action == 'weather.search':
-                return extractWeather(data)
-            elif action == 'wind.search':
-                return extractWind(data)
-        else:
-            return processErrorrequest(result.getcode())
-    else:
-        return processErrorrequest(206)
-
-
-def extractWind(result):
-    query = result['query']
-    if query:
-        result = query['results']
-        if result:
-            channel = result['channel']
-            if channel:
-                units = channel['units']['speed']
-                cityName = channel['location']['city']
-                cityLocation = channel['location']['city'] + "," + channel['location']['country']
-                windCondition = channel['wind']
-                speech = "Today in " + cityLocation + ' wind is flowing  with a speed of ' + \
-                         windCondition['speed'] + units
-                # return jsonify({'status': 'success', 'message': parsed_json[10]}), 200
-                return jsonify(
-                    {'speech': speech, 'displayText': speech, 'source': 'lakshman weather support from yahoo',
-                     'contextOut': [{'name': 'weather', 'lifespan': 2, 'parameters': {'city': cityName}}]}), 200
-            else:
-                return processErrorrequest(206, "fail in channel")
-        else:
-            return processErrorrequest(206, "fail in result")
-    return processErrorrequest(206, "fail in query")
-
-
-def makeyqlQuery(req):
-    city = getLocation(req)
-    if city:
-        return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-    else:
-        return processErrorrequest(206, "failed in makeyqlQuery fails")
-
-
-def processErrorrequest(code, msg):
-    return jsonify({'code': code, 'message': errorResponse(code, msg=msg)}), code
-
-
-def errorResponse(code, msg):
-    if code == 400:
-        return "400 BAD request"
-    elif code == 401:
-        return "401 Unauthorized"
-    elif code == 403:
-        return "403 Forbidden"
-    elif code == 404:
-        return "404 Not found"
-    elif code == 500:
-        return "500 Server fault"
-    elif code == 503:
-        return "503 server not available"
-    else:
-        return "webhook call failed with %code% error ", msg
-
-
-def extractWeather(result):
-    query = result['query']
-    if query:
-        result = query['results']
-        if result:
-            channel = result['channel']
-            if channel:
-                units = channel['units']['temperature']
-                cityName = channel['location']['city']
-                cityLocation = channel['location']['city'] + "," + channel['location']['country']
-                forecastForToday = channel['item']['condition']
-                forecastForFuture = channel['item']['forecast']
-                # attachContextOut = jsonify(
-                #     [{'name': 'weather', 'lifespan': 2, 'parameters': {'city': cityName}}])
-                for forecast in forecastForFuture:
-                    print(processForecast(forecast))
-                speech = "Today in " + cityLocation + ' weather is ' + forecastForToday[
-                    'text'] + ' with temperature of ' + \
-                         forecastForToday[
-                             'temp'] + units
-                # return jsonify({'status': 'success', 'message': parsed_json[10]}), 200
-                return jsonify(
-                    {'speech': speech, 'displayText': speech, 'source': 'lakshman weather support from yahoo',
-                     'contextOut': [{'name': 'weather', 'lifespan': 2, 'parameters': {'city': cityName}}]}), 200
-            else:
-                return processErrorrequest(206, "fail in channel")
-        else:
-            return processErrorrequest(206, "fail in result")
-    return processErrorrequest(206, "fail in query")
-
-
-def processForecast(forecast):
-    return forecast['day'] + "," + forecast['date'] + " highest " + forecast['high'] + " lowest " + forecast[
-        'low'] + " " + forecast['text']
